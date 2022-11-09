4af907f7f35378468b3df1b98db8052b3782e7e9
Ahmed Samir <107774249+Ahmedsamir28@users.noreply.github.com>
2022-11-09 03:40:55 +0200

    changed all the files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*Start Header*/
+
+// Strat Navbar 
+
+let navBar = document.getElementById('bar')   /* step -1 */
+function myFunction() {
+    navBar.classList.toggle("change");
+}
+
+  /* step -2 */
+
+function myFunction(x) {   /* add argument (this) in html to add before calsslist */
+    x.classList.toggle("change");
+}
+
+// End Navbar 
+
+/*End Header*/
