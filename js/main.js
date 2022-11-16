aa83c0097cb9891df50f81b62d4faa5c43414981
Ahmed Samir <107774249+Ahmedsamir28@users.noreply.github.com>
2022-11-16 07:06:29 +0200

    a

--- js/main.js
+++ js/main.js
@@ -19,8 +19,23 @@ function myFunction(x) {   /* add argument (this) in html to add before calsslis
 
 
 
+
+
 // let url = 'https://fakestoreapi.com/products';
 
 // fetch('https://fakestoreapi.com/products')
 //             .then(res=>res.json())
 //             .then(json=>console.log(json))
+
+/*  const url = "https://randomuser.me/api/?results=5";
+fetch(url).then( response => { return response.json()}).then(data =>{
+
+        console.log(data.results);
+        
+}).catch(error =>{
+        // هنا نضع اوامر الذي نرغب بتنفيذها عند حدوث خطا اثناء استرداد بيانات من  API
+
+    console.log(error);  // هنا طلبنا بعر ض الخطا الذي حدث اثناء استرداد البيانات 
+})
+
+*/
