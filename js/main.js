78391a8dc2142be41903b10d3ee9a0525831ff03
Ahmed Samir <107774249+Ahmedsamir28@users.noreply.github.com>
2022-11-18 01:49:16 +0200

    change

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 
 // Strat Navbar 
 
-let navBar = document.getElementById('bar')   
+let navBar = document.getElementById('bar')
 
 // function myFunction() {                    /* step -1 */
 //     navBar.classList.toggle("change");
 // }
 
-  /* step -2 */
+/* step -2 */
 
 // function myFunction(x) {   /* add argument (this) in html to add before calsslist */
 //     x.classList.toggle("change");
@@ -19,7 +19,7 @@ navBar.addEventListener('click', () => {    /*  step - 3*/
 })
 
 /* nav-menu */
-let linkes =  document.getElementById("linkes")
+let linkes = document.getElementById("linkes")
 navBar.addEventListener('click', () => {
     linkes.classList.toggle("navmenu");
 })
@@ -28,7 +28,7 @@ navBar.addEventListener('click', () => {
 let linkes_tag = document.querySelectorAll('.linkes li a')
 
 linkes_tag.forEach(el => {
-    el.addEventListener('click', ()=> {
+    el.addEventListener('click', () => {
         linkes_tag.forEach(li => {
             li.classList.remove('active')
             this.classList.add('active')
@@ -42,6 +42,17 @@ linkes_tag.forEach(el => {
 /*End Header*/
 
 // Start landing page
+let slide_landing = document.querySelector('.landing');
+let slide = document.querySelector('.slides');
+let nextBtn = document.getElementById('next-btn');
+let prevBtn = document.getElementById('prev-btn');
+
+let slides = document.querySelectorAll('.slide');
+let index = 1;
+let first_node = slides[0].cloneNode(true);
+let first_last = slides[slides.length -1].cloneNode(true);
+console.log(first_last)
+
 
 
 // End landing page
@@ -68,3 +79,85 @@ fetch(url).then( response => { return response.json()}).then(data =>{
 })
 
 */
+
+
+
+
+
+
+
+
+
+
+// const slideContainer = document.querySelector('.landing');
+// const slide = document.querySelector('.slides');
+// const nextBtn = document.getElementById('next-btn');
+// const prevBtn = document.getElementById('prev-btn');
+// const interval = 3000;
+
+// let slides = document.querySelectorAll('.slide');
+// let index = 1;
+// let slideId;
+
+// const firstClone = slides[0].cloneNode(true);
+// const lastClone = slides[slides.length - 1].cloneNode(true);
+
+// firstClone.id = 'first-clone';
+// lastClone.id = 'last-clone';
+
+// slide.append(firstClone);
+// slide.prepend(lastClone);
+
+// const slideWidth = slides[index].clientWidth;
+
+// slide.style.transform = `translateX(${-slideWidth * index}px)`;
+
+// console.log(slides);
+
+// const startSlide = () => {
+//     slideId = setInterval(() => {
+//         moveToNextSlide();
+//     }, interval);
+// };
+
+// const getSlides = () => document.querySelectorAll('.slide');
+
+// slide.addEventListener('transitionend', () => {
+//     slides = getSlides();
+//     if (slides[index].id === firstClone.id) {
+//         slide.style.transition = 'none';
+//         index = 1;
+//         slide.style.transform = `translateX(${-slideWidth * index}px)`;
+//     }
+
+//     if (slides[index].id === lastClone.id) {
+//         slide.style.transition = 'none';
+//         index = slides.length - 2;
+//         slide.style.transform = `translateX(${-slideWidth * index}px)`;
+//     }
+// });
+
+// const moveToNextSlide = () => {
+//     slides = getSlides();
+//     if (index >= slides.length - 1) return;
+//     index++;
+//     slide.style.transition = '.7s ease-out';
+//     slide.style.transform = `translateX(${-slideWidth * index}px)`;
+// };
+
+// const moveToPreviousSlide = () => {
+//     if (index <= 0) return;
+//     index--;
+//     slide.style.transition = '.7s ease-out';
+//     slide.style.transform = `translateX(${-slideWidth * index}px)`;
+// };
+
+// slideContainer.addEventListener('mouseenter', () => {
+//     clearInterval(slideId);
+// });
+
+// slideContainer.addEventListener('mouseleave', startSlide);
+// nextBtn.addEventListener('click', moveToNextSlide);
+// prevBtn.addEventListener('click', moveToPreviousSlide);
+
+// startSlide();
